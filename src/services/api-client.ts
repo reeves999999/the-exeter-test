6455afb8ed119ceb401830751318044a361acf93
reeves999999 <55240533+reeves999999@users.noreply.github.com>
2024-05-22 13:34:27 +0100

    set to use https

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://api.github.com/repos/',
+  baseURL: 'https://api.github.com/repos/',
 });
 
 class APIClient<T> {
